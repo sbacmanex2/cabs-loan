7637dce2001feb0e8406f10304adf06ab12ef4e5
thordoer <ivanmula744@gmail.com>
2026-06-13 14:15:19 -0400

    js

--- backend/server.js
+++ backend/server.js
@@ -93,7 +93,7 @@ async function registerAllWebhooks() {
 }
 
 // Call it when server starts
-registerAllWebhooks();
+// registerAllWebhooks();
 
 // ==================== HELPER FUNCTIONS ====================
 
